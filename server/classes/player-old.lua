92af44e4b88df0b43b0d45b7e78b1af6067c575d
guillerp <gui282828@gmail.com>
2022-02-07 16:54:10 +0100

    feat(core) more data and deprecated adv

--- server/classes/player-old.lua
+++ server/classes/player-old.lua
@@ -2,13 +2,21 @@
 -- We are planning on deprecating this method in the future as more members use Atlas framework. Please use the other functions instead.
 -- Please read the documentation on https://atlasfw.live/documentation, we will not provide support for this method.
 
----Get a player object
+---Get a player object **DEPRECATED**
 ---@param playerId number
 ---@return table
 ATL.GetPlayer = function(playerId)
     local player = Players[playerId]
     if not player then return error(("The player with id '%s' does not exist"):format(playerId)) end
     local self = { }
+
+    self.accounts = player.accounts
+    self.group = player.group
+    self.job_data = player.jobs
+    self.char_id = player.char_id
+    self.char_data = player.char_data
+    self.inventory = player.inventory
+    self.source = player.source
 
     self.setGroup = function(group)
         return player:SetGroup(group)
