de60535df90e9d1224686df14ed11355cb1d73db
German Fernandez <germanfernandez0306@gmail.com>
2022-02-22 13:08:59 -0300

    refactor(core): delete repetitive validation
    (with deferrals is enough)

--- server/functions/events.lua
+++ server/functions/events.lua
@@ -4,9 +4,6 @@ local function playerJoined(playerId)
     local playerId <const> = source or playerId
     if Players[playerId] then return DropPlayer(playerId, '[ATL] Player with same identifier is already logged in.') end
 
-    local license = ATL.GetLicense(playerId)
-    if not license then return DropPlayer(playerId, '[ATL] License not found. Please make sure you are using an official license. If you think this is an error, please contact the server owner.') end
-
     local characters = ATL.GetCharacters(license)
     TriggerClientEvent('atl:client:startMulticharacter', playerId, characters, Config.Identity)
     -- if Config.Identity.Disable then
@@ -19,9 +16,6 @@ local function registerPlayer(identity)
     local playerId <const> = source
     if Players[playerId] then return DropPlayer(playerId, '[ATL] Player with same identifier is already logged in.') end
     if type(identity) ~= 'table' then return DropPlayer(playerId, '[ATL] Invalid identity.') end
-
-    local license = ATL.GetLicense(playerId)
-    if not license then return DropPlayer(playerId, '[ATL] License not found. Please make sure you are using an official license. If you think this is an error, please contact the server owner.') end
 
     local newIdentity = ATL.CheckIdentity({
         firstname = identity.data.firstname,
