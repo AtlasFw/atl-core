9306c1c2d02555a0437bbfb6a569d9deeb5de235
Bombay <72895600+BombayV@users.noreply.github.com>
2022-02-26 07:21:51 -0800

    feat(data): added models

---
+++ data/models.lua
@@ -0,0 +1,9 @@
+local models <const> = {
+  { label = 'a_f_m_beach_01', value = 'a_f_m_beach_01' },
+  { label = 'mp_m_freemode_01', value = 'mp_m_freemode_01' },
+  { label = 'mp_f_freemode_01', value = 'mp_f_freemode_01' },  
+}
+
+exports('Models', function()
+  return models
+end)
