dc6e0b8ab1d5468f6f1ef1f11e07e1e5d8ada5e9
Bombay <OMGmsebastian@gmail.com>
2022-03-09 09:20:05 -0500

    refactor(player): remove useless functions

--- server/classes/player.lua
+++ server/classes/player.lua
@@ -1,6 +1,4 @@
--- If you plan working on the core, please use the following functions.
--- We also recommend you to not use something such as ATL.Players[source].identifier.
--- Instead of that use a getter method such as ATL.Players[source]:getIdentifier()
+-- If you plan working on the core, please use the following functions..
 
 local encode = json.encode
 local decode = json.decode
@@ -11,31 +9,49 @@ setmetatable(ATL.Players, player)
 --Sets the player's index to player's table
 player.__index = player
 
-ATL.new = function(source, identifier, char_id, player)
+ATL.new = function(source, identifier, char_id, data)
   local self = {}
   self.source = source
   self.identifier = identifier
   self.char_id = char_id
-  self.char_data = decode(player.char_data) or { coords = Config.Spawn }
-  self.job_data = decode(player.job_data) or {}
-  self.group = player.group or Config.Groups[1] or 'user'
-  self.slots = player.slots or Config.Identity.AllowedSlots
-  self.accounts = decode(player.accounts) or Config.Accounts
-  self.inventory = decode(player.inventory) or {}
-  self.status = decode(player.status) or Config.Status
-  self.appearance = decode(player.appearance) or {}
-  self.identity = decode(player.identity) or {}
-  ATL.Players[self.source] = self
+
+  -- Player data
+  self.accounts = decode(data.accounts) or Config.Accounts
+  self.appearance = decode(data.appearance) or {}
+  self.char_data = decode(data.char_data) or { coords = Config.Spawn }
+  self.group = data.group or Config.Groups[1] or 'user'
+  self.identity = decode(data.identity) or {}
+  self.inventory = decode(data.inventory) or {}
+  self.job_data = decode(data.job_data) or {}
+  self.slots = data.slots or Config.Identity.AllowedSlots
+  self.status = decode(data.status) or Config.Status
+
+  ATL.Players[source] = self
 
   TriggerClientEvent('atl:client:characterLoaded', source, self)
   return setmetatable(self, getmetatable(ATL.Players))
 end
 
---#region Getters
-function player:getIdentifier()
-  return self.identifier
+function player:savePlayer()
+  local ped = GetPlayerPed(self.source)
+  local coords, heading = GetEntityCoords(ped), GetEntityHeading(ped)
+  self:setCoords(vector4(coords.x, coords.y, coords.z, heading))
+
+  local queries = {
+    { query = 'UPDATE `users` SET `group` = ?, `slots` = ? WHERE `license` = ?', values = { self.group, self.slots, self.identifier }},
+    { query = 'UPDATE `characters` SET accounts = ?, status = ?, inventory = ?, job_data = ?, char_data = ? WHERE `char_id` = ? ', values = { encode(self.accounts), encode(self.status), encode(self.inventory), encode(self.job_data), encode(self.char_data), self.char_id }}
+  }
+
+  MySQL.Async.transaction(queries, function(result)
+    if result then
+      print('[ATL] User and Character ' .. self.source .. ' saved successfully')
+    end
+  end)
 end
 
+
+--#region Getters
+-- Player data
 function player:getCharId()
   return self.char_id
 end
@@ -44,14 +60,26 @@ function player:getSlots()
   return self.slots
 end
 
-function player:getCharacterName()
-  return self.identity.firstname, self.identity.lastname
+-- Character data
+function player:getCharName()
+  return {
+    firstname = self.identity.firstname,
+    lastname = self.identity.lastname
+  }
 end
 
+-- Group
 function player:getGroup()
   return self.group
 end
 
+function player:hasPerms(group)
+  if type(group) ~= 'string' then return false end
+  if not Config.Groups[group] then return false end
+  return Config.Groups[self.group] >= Config.Groups[group]
+end
+
+-- Jobs
 function player:getJobLabel()
   return Jobs[self.job_data.name].name
 end
@@ -65,6 +93,7 @@ function player:getDuty()
   return self.job_data.onDuty
 end
 
+-- Account
 function player:getAccount(account)
   if type(account) ~= 'string' then return false end
 
@@ -72,16 +101,28 @@ function player:getAccount(account)
     return self.accounts[account]
   end
 end
-
-function player:hasPerms(group)
-  if type(group) ~= 'string' then return false end
-  if not Config.Groups[group] then return false end
-  return Config.Groups[self.group] >= Config.Groups[group]
-end
-
 --#endregion Getters
 
 --#region Setters
+-- Player data
+function player:setCoords(coords, now)
+  if type(coords) ~= 'vector4' then return false end
+  if now then
+    SetEntityCoords(GetPlayerPed(self.source), coords.x, coords.y, coords.z)
+  end
+
+  self.char_data.coords = vec4(coords.x, coords.y, coords.z, coords.w)
+  return true
+end
+
+function player:setSlots(slots)
+  if type(slots) ~= 'number' then return false end
+
+  self.slots = self.slots + slots
+  return true
+end
+
+-- Group
 function player:setGroup(group)
   if type(group) ~= 'string' then return false end
   if not Config.Groups[group] then return false end
@@ -91,6 +132,7 @@ function player:setGroup(group)
   return true
 end
 
+-- Jobs
 function player:setJob(name, level)
   if type(name) ~= 'string' or type(level) ~= 'number' then return false end
 
@@ -111,23 +153,7 @@ function player:setDuty(bool)
   return true
 end
 
-function player:setCoords(coords, now)
-  if type(coords) ~= 'vector4' then return false end
-  if now then
-    SetEntityCoords(GetPlayerPed(self.source), coords.x, coords.y, coords.z)
-  end
-
-  self.char_data.coords = vec4(coords.x, coords.y, coords.z, coords.w)
-  return true
-end
-
-function player:setSlots(slots)
-  if type(slots) ~= 'number' then return false end
-
-  self.slots = self.slots + slots
-  return true
-end
-
+-- Accounts
 function player:addAccountMoney(account, quantity)
   if type(account) ~= 'string' or type(account) ~= 'number' then return false end
   if not self.accounts[account] then return false end
@@ -143,22 +169,4 @@ function player:removeAccountMoney(account, quantity)
   self.accounts[account] = self.accounts[account] - quantity
   return true
 end
-
 --#endregion Setters
-
-function player:savePlayer()
-  local ped = GetPlayerPed(self.source)
-  local coords, heading = GetEntityCoords(ped), GetEntityHeading(ped)
-  self:setCoords(vector4(coords.x, coords.y, coords.z, heading))
-
-  local queries = {
-    { query = 'UPDATE `users` SET `group` = ?, `slots` = ? WHERE `license` = ?', values = { self.group, self.slots, self.identifier }},
-    { query = 'UPDATE `characters` SET accounts = ?, status = ?, inventory = ?, job_data = ?, char_data = ? WHERE `char_id` = ? ', values = { encode(self.accounts), encode(self.status), encode(self.inventory), encode(self.job_data), encode(self.char_data), self.char_id }}
-  }
-
-  MySQL.Async.transaction(queries, function(result)
-    if result then
-      print('[ATL] User and Character ' .. self.source .. ' saved successfully')
-    end
-  end)
-end
