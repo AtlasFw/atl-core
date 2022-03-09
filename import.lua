4abc8a5ddb64fbab49bd3aaf0c0a10d5dc3c2836
BombayV <OMGmsebastian@gmail.com>
2022-03-09 18:36:13 -0500

    change to function

--- import.lua
+++ import.lua
@@ -1,13 +1,23 @@
 ATL = exports['atl-core']:get()
 
-Test = {}
-local metatable = {
-  __index = function(self, name)
-    -- self prints out the table
-    -- name prints out the function name that was called
-    print(self, name)
-    return
-  end
-}
+-- Test = {}
+-- local metatable = {
+--   __index = function(self, name)
+--     print(self, name)
+--     return
+--   end
+-- }
 
-setmetatable(Test, metatable)
+-- setmetatable(Test, metatable)
+
+function Test()
+  local b = {}
+  local metatable = {
+    __index = function(self, name)
+      print(self, name)
+      return
+    end
+  }
+  setmetatable(b, metatable)
+  return b
+end
