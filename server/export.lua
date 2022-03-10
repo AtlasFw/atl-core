eac156a425abe5a85920825d3f57e5a9dcecc33b
BombayV <OMGmsebastian@gmail.com>
2022-03-09 19:41:23 -0500

    remove old player methods

--- server/export.lua
+++ server/export.lua
@@ -1,3 +1,7 @@
+-- Don't use this export. Use the import.
+-- If you need further help read the documentation.
+-- https://atlasfw.live/documentation
+
 exports('get', function()
   local resourceName = GetInvokingResource()
   if not ATL.Resources[resourceName] then
