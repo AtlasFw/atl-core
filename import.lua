010386c4c8490622add861a1fabad7202c3c18a5
BombayV <OMGmsebastian@gmail.com>
2022-03-09 19:15:11 -0500

    somewhat of a working player method?

--- import.lua
+++ import.lua
@@ -1,21 +1,18 @@
+-- Due to the way exports work, metatables are not preserved when importing.
+-- This means that the only way to get the methods of the player "class" from
+-- another resource is to import the file and then use the function provided below.
+
 ATL = exports['atl-core']:get()
 
--- Test = {}
--- local metatable = {
---   __index = function(self, name)
---     print(self, name)
---     return
---   end
--- }
-
--- setmetatable(Test, metatable)
-
-function Test()
-  local b = {}
+function Test(player)
+  local b = player
   local metatable = {
     __index = function(self, name)
-      print(self, name)
-      return
+      return function(...)
+        print(self, name, ...)
+        print(debug.getinfo((exports['atl-core']['atl_' .. name]), 'u').nparams)
+        return exports['atl-core']['atl_' .. name](nil, self, ...)
+      end
     end
   }
   setmetatable(b, metatable)
