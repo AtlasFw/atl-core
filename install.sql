7c04a3a9d11e8d632c10ca32d0e5b9de3be25eab
BombayV <OMGmsebastian@gmail.com>
2022-04-12 18:22:47 -0400

    tweak(sql): create database

--- install.sql
+++ install.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `atlasfw`;
+USE `atlasfw`;
 CREATE TABLE `users` (
   `license` VARCHAR(60) NOT NULL,
   `name` VARCHAR(60) NOT NULL,
@@ -5,7 +7,6 @@ CREATE TABLE `users` (
   `slots` INTEGER(2) NOT NULL,
   PRIMARY KEY (`license`)
 ) DEFAULT CHARSET UTF8;
-
 CREATE TABLE `characters` (
   `char_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `license` VARCHAR(60) NOT NULL,
