8c60f3169f582dbdd678ceda7a26d256129e55ab
D4NTE-4LEX <dannyjacome78gmail.com>
2022-04-23 21:49:02 -0500

    feat(locales/en): appearance keys

--- data/locales/en.lua
+++ data/locales/en.lua
@@ -2,6 +2,48 @@ return {
   ['core'] = 'Welcome',
   ['error'] = 'Error',
   ['appearance'] = {
-    ['ped_label'] = 'Ped Model View'
+    ['ped_view'] = 'Ped Model View',
+    ['ped_model'] = 'Model',
+
+    ['acc_view']= 'Accessories View',
+    ['acc_glasses']= 'Glasses',
+    ['acc_mask']= 'Mask',
+    ['acc_bags']= 'Bags & Paracute',
+    ['acc_chains']= 'Scarf & Chains',
+    ['acc_watches']= 'Watches',
+    ['acc_bracelets']= 'Bracelets',
+
+    ['face_view']= 'Face View',
+    ['face_eyebrows']= 'Eyebrowns',
+    ['face_eyes&mount']= 'Eyes & Mouth',
+    ['face_nose']= 'Nose',
+    ['face_jaw']= 'Jaw',
+    ['face_chin']= 'Chin',
+    ['face_cheeks']= 'Cheeks',
+    ['face_neck']= 'Neck',
+    
+    ['head_view']= 'Head View',
+    ['head_hair']= 'Hair',
+    ['head_eyebrows']= 'Head View',
+    ['head_eyecolor']= 'Head View',
+    ['head_view']= 'Head View',
+    ['head_view']= 'Head View',
+    ['head_view']= 'Head View',
+    ['head_view']= 'Head View',
+    ['head_view']= 'Head View',
+
+
+    ['inherit_view']= 'Family Inheritance View',
+
+
+    ['lower_view']= 'Lower Body View',
+
+
+    ['tattoos_view']= 'Tattoos View',
+
+
+    ['upper_view']= 'Upper Body View',
+    
+
   }
 }
