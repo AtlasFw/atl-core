50621e301cae33c27211ad6b4fb40007db7499b3
Zuntie <60542183+Zuntie@users.noreply.github.com>
2022-05-11 21:41:25 +0200

    Danish Translation
    I've added danish translations.

---
+++ data/locales/da.lua
@@ -0,0 +1,44 @@
+return {
+  ['core'] = 'Velkommen',
+  ['error'] = 'Fejl',
+  ['appearance'] = {
+    ['ped_view'] = 'Ped Model Visning',
+    ['ped_model'] = 'Model',
+
+    ['acc_view']= 'Tilbehørs Visning',
+    ['acc_glasses']= 'Briller',
+    ['acc_mask']= 'Masker',
+    ['acc_bags']= 'Tasker & Faldskærm',
+    ['acc_chains']= 'Halstørklæde & Halskæde',
+    ['acc_watches']= 'Ur',
+    ['acc_bracelets']= 'Armbånd',
+
+    ['face_view']= 'Angsigts Visning',
+    ['face_eyebrows']= 'Øjenbryn',
+    ['face_eyes&mount']= 'Øjne & Mund',
+    ['face_nose']= 'Næse',
+    ['face_jaw']= 'Kæbe',
+    ['face_chin']= 'Hage',
+    ['face_cheeks']= 'Kinder',
+    ['face_neck']= 'Nakke',
+    
+    ['head_view']= 'Hoved Visning',
+    ['head_hair']= 'Hår',
+    ['head_eyebrows']= 'Hoved Visning',
+    ['head_eyecolor']= 'Hoved Visning',
+
+
+    ['inherit_view']= 'Familiearvesyn',
+
+
+    ['lower_view']= 'Underkrops Visning',
+
+
+    ['tattoos_view']= 'Tatoveringer Visning',
+
+
+    ['upper_view']= 'Overkrops Visning',
+    
+
+  }
+}
