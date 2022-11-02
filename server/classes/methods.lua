5355a99b7e9122342221aa6d720ceca64f306927
Bombay <OMGmsebastian@gmail.com>
2022-11-02 15:32:01 -0400

    a

--- server/classes/methods.lua
+++ server/classes/methods.lua
@@ -1,3 +1,11 @@
 function Player:add(a)
   self.money = self.money + a
 end
+
+function Player:logout(dropped)
+  if dropped then
+    print("Player " .. self.name .. " has been dropped")
+  else
+    print("Player " .. self.name .. " has been logged out")
+  end
+end
--- server/main.lua
+++ server/main.lua
@@ -1,12 +1,26 @@
 ATL, PlayerList = {}, {}
 _G.PlayerList, _G.ATL = PlayerList, ATL
 
-AddEventHandler('playerJoining', function(name, setKickReason, deferrals)
+local tempId
+
+AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
   deferrals.defer()
-  deferrals.update("Loading...")
+  deferrals.update("[Atlas] Checking player...")
+  local playerId <const> = source
+  local identifier = GetIdentifier(playerId, 'license')
+  if not identifier then
+    deferrals.done("[Atlas] No identifier found")
+    CancelEvent()
+    return
+  end
+
   deferrals.done()
 end)
 
 AddEventHandler('playerDropped', function(reason)
-  print("playerDropped", reason)
+  local playerId <const> = source
+  local player = PlayerList[playerId]
+  if player then
+    player:logout(true)
+  end
 end)
